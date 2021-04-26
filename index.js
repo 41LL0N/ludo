ab70259a75b1dda1716c5fa0f5963da2bf7782ed
Dillon <dillonveit1@gmail.com>
2021-04-27 09:19:59 +1000

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ await mongoose.connect("mongodb+srv://admin:eschooladmin@cluster0.poe1m.mongodb.
         useFindAndModify: false,
 })
 
-    await new App().listen(8080);
+    await new App().listen(process.env.PORT || 8080);
     console.log("Connected to the server. Website is operational!")
 })();
